5d4d819a16900fdfa067e316d45251d1ac925ae6
FernandaMascarenhas <fer.mascarenhass@gmail.com>
2024-05-12 00:29:28 -0400

    carrossel funcional e inacabado

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,25 +1,74 @@
 <template>
-    <section class="carousel">
-        <div class="icon__left">
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#259172" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
-                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
-            </svg>
-        </div>
-
-        <img class="carousel__img" src="../assets/images/coraline.jpg" alt="Imagem Capa">
-
-        <div class="carousel__info">
-            <div class="carousel__info-titulo">Título da Obra</div>
-            <div class="carousel__info-conteudo">Enquanto explora sua nova casa à noite, a pequena Coraline descobre uma porta secreta que contém um mundo parecido com o dela, porém melhor em muitas maneiras. Todos têm botões no lugar dos olhos, os pais são carinhosos e os sonhos de Coraline viram realidade por lá. Ela se encanta com essa descoberta, mas logo percebe que segredos estranhos estão em ação: uma outra mãe e o resto de sua família tentam mantê-la eternamente nesse mundo paralelo.</div>
-        </div>
-
-        <div class="icon__right">
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#259172" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
-            </svg>
-        </div>
-    </section>
+  <div id="carousel" class="carousel slide">
+    <div class="carousel-inner">
+		<div class="carousel-item active">
+			<div class="carousel__wrapper">
+				<div class="col-md-6">
+					<img src="../assets/images/coraline.jpg" class="carousel__img" alt="...">
+				</div>
+				<div class="col-md-6">
+					<div class="carousel-caption d-none d-md-block">
+						<div class="carousel__titulo">First slide label</div>
+						<div class="carousel__sinopse">Some representative placeholder content for the first slide.</div>	
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<div class="carousel__wrapper">
+				<div class="col-md-6">
+					<img src="../assets/images/coraline.jpg" class="carousel__img" alt="...">
+				</div>
+				<div class="col-md-6">
+					<div class="carousel-caption d-none d-md-block">
+						<div class="carousel__titulo">Second slide label</div>
+						<div class="carousel__sinopse">Some representative placeholder content for the second slide.</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<div class="carousel__wrapper">
+				<div class="col-md-6">
+					<img src="../assets/images/coraline.jpg" class="carousel__img" alt="...">
+				</div>
+				<div class="col-md-6">
+					<div class="carousel-caption d-none d-md-block">
+						<div class="carousel__titulo">Third slide label</div>
+						<div class="carousel__sinopse">Some representative placeholder content for the third slide.</div>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+    <button class="carousel-control-prev" type="button" @click="prevSlide">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" @click="nextSlide">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    nextSlide() {
+      const carousel = document.getElementById('carousel');
+      const instance = new bootstrap.Carousel(carousel);
+      instance.next();
+    },
+    prevSlide() {
+      const carousel = document.getElementById('carousel');
+      const instance = new bootstrap.Carousel(carousel);
+      instance.prev();
+    }
+  }
+}
+</script>
+
 
 
 <style>
@@ -35,6 +84,9 @@
 .carousel__wrapper {
     display: flex;
     flex-direction: row;
+	margin: 0 auto;
+	max-width: 80%;
+	align-items: center;
 }
 
 .carousel__img {
@@ -42,27 +94,24 @@
     margin: 1.5rem;
 }
 
-.carousel__info {
-    margin: 1.5rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-}
-
-.carousel__info-titulo {
+.carousel__titulo {
+	color: black;
     font-family: "Inter", sans-serif;
     font-weight: bolder;
     font-size: larger;
     margin-bottom: 1rem;
 }
 
-.carousel__info-conteudo {
+.carousel__sinopse {
+	color: black;
     font-family: "Inter", sans-serif;
     font-weight: normal;
     font-size: smaller;
-    text-align: justify;
-    width: 18rem;
+}
+
+.carousel-control-prev-icon,
+.carousel-control-next-icon {
+    color: black !important;
 }
 
 </style>
