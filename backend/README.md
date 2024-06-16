e9f559d618784498cfaa0b2f4792a3ebfd1b1274
Vitor Yuske <vitor.yuski1@gmail.com>
2024-06-16 15:36:43 -0400

    Adding backend files

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,2 @@
 # opinarte-aplicativo-web-backend
+
---
+++ backend/src/main/java/br/ufms/facom/opinarte_backend/OpinarteBackendApplication.java
@@ -0,0 +1,13 @@
+package br.ufms.facom.opinarte_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpinarteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpinarteBackendApplication.class, args);
+	}
+
+}
